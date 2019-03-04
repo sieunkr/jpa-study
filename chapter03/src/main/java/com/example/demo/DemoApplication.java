d53ef55cf3bcb8cb51bbf1209b0e95db53ad3eca
sieunkim <sieunkr@gmail.com>
2019-03-04 22:48:11 +0900

    JPA 3장

---
+++ chapter03/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication implements CommandLineRunner {
+
+    private final UserDao userDao;
+
+    public DemoApplication(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+    }
+}
---
+++ chapter03/src/main/java/com/example/demo/UserDao.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public class UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public User getByEmail(String email){
+        return entityManager.find(User.class, email);
+    }
+
+    //TODO:트랜잭션 추가
+    public List<User> getByName(String name){
+
+        TypedQuery<User> query =
+                entityManager.createQuery(
+                        "select u from User u order by u.name",
+                        User.class);
+        List<User> result = query.getResultList();
+        return result;
+
+    }
+
+    //TODO:트랜잭션
+    public void changeName(String email, String name){
+
+        User user = entityManager.find(User.class, email);
+        user.changeName(name);
+
+    }
+
+}
